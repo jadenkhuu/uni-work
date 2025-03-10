ae6a18e4e42c5b2532d71f6bcdd5afebb78379aa
z5416824 <z5416824@ad.unsw.edu.au>
2025-03-10 14:06:19 +1100

    completed theory

--- COMP6991/lab02/collections/src/main.rs
+++ COMP6991/lab02/collections/src/main.rs
@@ -19,18 +19,34 @@ fn main() {
 
     // TODO: your text explanation to the questions in the spec
     // Which collection type was the fastest for adding and removing elements?
-        // The fastest seems to be Vector, when both insert and remove are summed
+        // - The fastest seems to be Vector, when both insert and remove are summed
     // Why do you think this was the case?
-        
+        // - Inserting and removing from the end of a vec is usually fastest with O(1) time
+        // - It only needs one operation instead of having to iterate through and add somewhere
+        // - i.e A list would be more useful for inserting/removing from the middle of a list
 
     // Is there any significant difference between Vec and VecDeque deletion?
+        // VecDeque is way faster than Vec in terms of removal
     // If so, why? If not, why not?
+        // - VecDeque removes from the front instead which has a time of O(1).
+        // - When Vec removes, it needs to shift over all the remaining elements 
+        // which gives it a time of O(n)
 
     // When would you consider using VecDeque over Vec?
+        // - You would use a VecDeque when you want a Vec which supports efficient insertion
+        // at both ends of the sequence.
+        // - If you want a queue.
+        // - if you want a dobule ended queue
 
     // When would you consider using LinkedList over Vec?
+        // - When you want a Vec but dont know what the size needs to be
+        // - For efficient split and appending
 
     // Did the results suprise you? Why or why not?.
+        // - The results surprised me a little, seeing how long HashMaps took to insert/remove
+        // - However, I already expected Vec and VecDeque to have fastest insertion/removal
+        // with the idea that it will likely have the slowest look up speeds compared to 
+        // linked lists and hash maps
 }
 
 // measure the insertion and removal
